4e3e07cc6b9d1aad7a9a5ac9f5622bd42db69501
Sean Chen <sean.chen@leocorn.com>
2014-10-17 12:22:39 -0400

    Here is the prototype for handling base64 data upload.

--- SpecialPlupload.php
+++ SpecialPlupload.php
@@ -8,32 +8,29 @@ class SpecialPlupload extends SpecialPage {
 
     /**
      *
-     * Theck this page about how to take over output in a Special page.
+     * Theck this page about how to take over output 
+     * in a Special page.
      * http://www.mediawiki.org/wiki/Taking_over_output_in_your_special_page
      */
     function execute( $par ) {
 
-        $action = $this->getRequest()->getText('action');
-        if($action != 'plupload') {
+        $request = $this->getRequest();
+        $action = $request->getText('action');
+        if($action != 'plupload' && $action != 'base64') {
             // this is not a PLupload request, print out
             // help info and return.
             $this->printHelp();
             return;
         }
+        // anonymous user verification.
+        // anonymous user as 0 as id, we try to block anonymous user
+        // to upload.
+        $userId = $this->getUser()->getId();
+        if($userId <= 0) {
+            // this is anonymous user, check the settings for 
+            // anonymous user.
+        }
 
-        // this is a PLupload request, disable the MediaWiki output.
-        global $wgOut;
-        $wgOut->disable();
-
-        $result = $this->handlePlupload();
-
-        echo(json_encode($result));
-        die();
-    }
-
-    function handlePlupload() {
-
-        $request = $this->getRequest();
         // get ready wiki text.
         $pageText = $this->getPageText($request);
         $comment = $this->getPageComment($request);
@@ -42,14 +39,38 @@ class SpecialPlupload extends SpecialPage {
         // get the desired destination file name.
         $fileName = $request->getText("wpDestFile");
 
+        // this is a PLupload request, disable the MediaWiki output.
+        global $wgOut;
+        $wgOut->disable();
+
+        switch($action) {
+        case 'plupload':
+            $result = $this->handlePlupload($request, $pageText, 
+                                            $comment, $watch, 
+                                            $fileName);
+            break;
+        case 'base64':
+            $result = $this->handleBase64($request, $pageText, 
+                                          $comment, $watch, 
+                                          $fileName);
+            break;
+        }
+
+        echo(json_encode($result));
+        die();
+    }
+
+    /**
+     * handle plupload request.
+     */
+    function handlePlupload($request, $pageText, $comment,
+                            $watch=true, $fileName) {
+
         $mUpload = UploadBase::createFromRequest($request);
 
         // user verification! using the UploadBase 
-        $userId = $this->getUser()->getId();
         $permit = $mUpload->verifyTitlePermissions($this->getUser());
-        // anonymous user as 0 as id, we try to block anonymous user
-        // to upload.
-        if($userId > 0 && $permit) {
+        if($permit) {
             // user is allow to upload...
 
             $mLocalFile = $mUpload->getLocalFile();
@@ -80,6 +101,34 @@ class SpecialPlupload extends SpecialPage {
     }
 
     /**
+     * handle Base64 request.
+     */
+    function handleBase64($request, $pageText, $comment, 
+                          $watch=true, $fileName) {
+
+        // get the base64 data: data:image/png;base64,
+        // assume the base64Data has NO encoding prefix.
+        $base64Data = $request->getText('base64Data');
+        $fileFullPath = $this->dumpTemp(base64_decode($base64Data));
+        //$fileFullPath = $this->dumpTemp($base64Data);
+        // create local file.
+        $file = wfLocalFile($fileName);
+        $file->upload($fileFullPath, $comment, $pageText, 0, false,
+                      false, $this->getUser());
+        // preparing the result.
+        $result = array(
+          "jsonrpc" => "2.0",
+          "success" => true,
+          "fileName" => $fileName,
+          "fileUrl" => $file->getCanonicalUrl(),
+          "pageUrl" => $file->getTitle()->getFullUrl(),
+          "mimeType" => $file->getMimeType()
+        );
+
+        return $result;
+    }
+
+    /**
      * preparing the page text for this uploaded file.
      */
     function getPageText($request) {
@@ -100,6 +149,17 @@ class SpecialPlupload extends SpecialPage {
     }
 
     /**
+     * @param $contents
+     * @return string
+     */
+    private function dumpTemp( $contents ) {
+
+        $filename = tempnam( wfTempDir(), 'importupload' );
+        file_put_contents( $filename, $contents );
+        return $filename;
+    }
+
+    /**
      * print out the brief help information for how to use this
      * special page.
      */
@@ -115,14 +175,18 @@ class SpecialPlupload extends SpecialPage {
 This is a special page to provide a simple way for PLupload 
 client to save files as MediaWiki Files.
 
-The following is a sample for '''multipart_params''' option from PLupload library. 
-The '''action''' has to be '''plupload''' to trigger the process to save file as MediaWiki File.
+The following is a sample for '''multipart_params''' option 
+from PLupload library. 
+The '''action''' has to be '''plupload''' to trigger the process 
+to save file as MediaWiki File.
 
 <source lang="javascript">
     multipart_params = {
-        action : "plupload",
+        action : "plupload" or "base64"
+        base64Data : "binary file in base64 format"
         desc : "testing upload from plupload [[Testing]]",
-        commment : "here is some comments"
+        commment : "here is some comments",
+        wpDestFile : 'The file name, which will be the page Title'
     }
 </source>
 
@@ -130,6 +194,8 @@ Here are the definition for each param:
 
 ; action
 : Only the action '''plupload''' will trigger the process to save file as the MediaWiki file.
+; base64Data
+: Only needed for action '''base64''', it will store the binary file in Base64 format.
 ; desc
 : this param will save as the summary text for the MediaWiki file.
 ; comment
